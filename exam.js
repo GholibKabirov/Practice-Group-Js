ac1d82f691c8faef850546310793e21d436631ba
GholibKabirov <kabiroovss123@gmail.com>
2022-06-08 15:16:27 +0300

    comit 2

--- exam.js
+++ exam.js
@@ -1,14 +1,12 @@
 let arr = [9,7,6,5,4,3,2,1];
 
-function f(array, num){
-    let arr1=[]
-    for(let i=0;i<array.length;i++){
-        arr1.push(array[i])
-        if(array[i]>=num ){arr1.push(num);}
-    }
-    return arr1;
+function f(aray, num){
+   let  arr1=[];
+   for(let i=0; i<aray.length; i++){
+       arr1.push(aray[i])
+       if(aray[i]>num && aray[i+1]<num  ){arr1.push(num);}
+   }
+ return arr1;
 }
-
-
 
 console.log(f(arr, 8))
